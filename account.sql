3c6ddbfd2e77d950777e9c2f364cab35cee99a5c
BranKSan22 <32780020+BranKSan22@users.noreply.github.com>
2017-12-10 17:40:56 -0600

    Update account.sql

--- account.sql
+++ account.sql
@@ -1,10 +1,15 @@
 DROP TABLE IF EXISTS account;
 
-/* This is a very simple table for a mysql-php example */
+/*This table creates an account for the user, allowing them to enter a username, password, email, and type of Iowa City community member they are*/
 CREATE TABLE account (
     id INT NOT NULL AUTO_INCREMENT,
     hashedpass VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    propic VARCHAR(255), /*figure out how to autoload a "blank" profile pic if the user decides to not use one of their own*/
+	email VARCHAR(300) NOT NULL,
+	typeperson VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
 );
+
+INSERT INTO account (hashedpass, username, email, typeperson) values ('$2a$12$s7h33TRR.wjhdKVdNIGON.pBVHlr2rancX6TxExL2UpLjTu.0TdbK', 'bksanders', 'brandy-sanders@uiowa.edu', 'Student');
+INSERT INTO account (hashedpass, username, email, typeperson) values ('$2a$12$4W4ej8SBkW6Z7wwEnG885OFemfYEPBTD1abWdg9ep0DqvZGc8LwyO','dwyrwas','duncan-wyrwas@uiowa.edu','Student');
+INSERT INTO account (hashedpass, username, email, typeperson) values ('$2a$12$FqVuPRs7P3A3mlzneMnzC.FRUB6bLqk6mhGpQT00XY7kdD1.XZSuq','spandya', 'sanjana-pandya@uiowa.edu', 'Student');                                             
